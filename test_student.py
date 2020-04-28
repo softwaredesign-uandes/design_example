bed4ace2858b3c73eb0caf061e27852b4be43ef1
Alejandro Echeverría <alejandro@echev.com>
2020-04-28 17:41:34 -0400

    feat: add support for floating point grades

--- test_student.py
+++ test_student.py
@@ -12,5 +12,8 @@ class TestStudent(unittest.TestCase):
   def test_check_assignment_grades_when_one_grade_and_incorrect_average_returns_false(self):
     self.assertEqual(Student([1], 10, 'a').check_assignment_grades(), False)
 
-  def test_check_assignment_grades_when_thre_grades_and_correct_average_returns_true(self):
+  def test_check_assignment_grades_when_three_grades_and_correct_average_returns_true(self):
     self.assertEqual(Student([1,1,4], 2, 'a').check_assignment_grades(), True)
+
+  def test_check_assignment_grades_when_three_float_grades_and_correct_average_returns_true(self):
+    self.assertEqual(Student([4.0,4.1,4.2], 4.1, 'a').check_assignment_grades(), True)
